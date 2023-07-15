36d8f46b7bec83ae36184953c14b94e224cec0d6
SnakeDhyan <98876616+SnakeDhyan@users.noreply.github.com>
2023-07-15 10:33:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+function imagsra(){
+    window.location="imagsra.html";
+}
+function bon(){
+    window.location="bon.html";
+}
+function dem(){
+    window.location="dem.html";
+}
+function sh(){
+    window.location="sh.html";
+}
+function rad(){
+    window.location="rad.html"
+}
+function just(){
+    window.location="just.html"
+}
+function gho(){
+    window.location="gho.html"
+}
+function ba(){
+    window.location="ba.html"
+}
+function sta(){
+    window.location="sta.html"
+}
+function sg(){
+    window.location="sg.html"
+}
+function cb(){
+    window.location="cb.html"
+}
+function edsh(){
+    window.location="edsh.html"
+}
+function toh(){
+    window.location="toh.html"
+}
+function bah(){
+    window.location="bah.html"
+}
+function shi(){
+    window.location="shi.html"
+}
+function per(){
+    window.location="per.html"
+}
+function soy(){
+    window.location="soy.html"
+}
